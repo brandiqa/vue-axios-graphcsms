232d5035d58630c457b4a65138e36d5014df8843
Michael Wanyoike <micwanyoike@gmail.com>
2018-11-23 13:04:07 +0300

    built comment form ui

--- src/components/CommentForm.vue
+++ src/components/CommentForm.vue
@@ -1,12 +1,39 @@
 <template>
   <section class="comment-form">
-    <p>Comment Form</p>
+    <b-form @submit.prevent="onSubmit">
+      <b-form-group label="Name">
+        <b-form-input id="input-name" type="text" v-model="name" placeholder="Enter your name" required></b-form-input>
+      </b-form-group>
+       <b-form-group label="Message">
+         <b-form-textarea id="input-message"
+                          v-model="message"
+                          placeholder="Enter your comment"
+                          :rows="3"
+                          :max-rows="6"
+                          required>
+          </b-form-textarea>
+      </b-form-group>
+      <b-button type="submit" variant="primary">Submit</b-button>
+    </b-form>
   </section>
 </template>
 
 <script>
 export default {
-
+  name: "CommentForm",
+  props: ['post'],
+  data() {
+    return {
+      name: '',
+      message: ''
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log(this.post)
+      console.log(this.name, this.message);
+    }
+  }
 }
 </script>
 
